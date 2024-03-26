46d99eecfa4c37704f36dfecf4a8d6d26954a236
TamNhien <canhthien91@gmail.com>
2024-03-26 19:34:19 +0700

    Add project files.

---
+++ P_InTuDauTienCuaChuoi.py
@@ -0,0 +1,4 @@
+﻿user_input = input("Nhập chuỗi : ")
+first_word = user_input.split()[0] if user_input.strip() else None
+print("Từ đầu tiên trong chuỗi là : ", first_word if first_word else "Chuỗi không có từ.")
+
